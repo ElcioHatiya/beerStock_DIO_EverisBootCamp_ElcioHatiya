3ebd1d226466c48aa35800e6cf8cb7c68fcecfa1
elcio <elciohatiya@outlook.com>
2021-11-12 11:42:36 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/beerStockAPIDIOElcioHatiya/BeerStockApiDioElcioHatiyaApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerStockAPIDIOElcioHatiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStockApiDioElcioHatiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStockApiDioElcioHatiyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/beerStockAPIDIOElcioHatiya/BeerStockApiDioElcioHatiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerStockAPIDIOElcioHatiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApiDioElcioHatiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
